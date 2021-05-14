428bf5d32d8cee24fcfdfc44bb7b17b8ef2cb4eb
mdtechs <mohddanish8299@gmail.com>
2021-05-14 15:38:48 +0530

    remove unused code

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 function togglePage(pos) {
+    
     let i = 3;
     while (i > 0) {
-        navElement[i - 1].classList.remove('active');
-        i--;
+      navElement[i - 1].classList.remove("active");
+      i--;
     }
-    var scroll = "translateY(-" + (pos - 1) * 100 + "%)";
-    page[0].style.transform = scroll;
-    navElement[pos - 1].classList.add('active');
+  var scroll = "translateY(-" + (pos - 1) * 100 + "%)";
+  page[0].style.transform = scroll;
+  navElement[pos - 1].classList.add("active");
+}
+function toggleIcon(pos){
 }
 
-let navElement = document.querySelectorAll('div.navEl'),
-    page = document.getElementsByClassName('pageContainer'),
-    pos = 1,
-    i = 1;
+let navElement = document.querySelectorAll("div.navEl"),
+  page = document.getElementsByClassName("pageContainer"),
+  pos = 1,
+  i = 1;
 
-Array.from(navElement).forEach(function(e) {
-    e.addEventListener("click", function() {
-        //navElement[pos].classlist.remove('active');
-        pos = this.dataset.pos;
-        if (pos == '1') togglePage(pos);
-        if (pos == '2') togglePage(pos);
-        if (pos == '3') togglePage(pos);
-    });
+Array.from(navElement).forEach(function (e) {
+  e.addEventListener("click", function () {
+    //navElement[pos].classlist.remove('active');
+    pos = this.dataset.pos;
+    if (pos == "1") togglePage(pos);
+    if (pos == "2") togglePage(pos);
+    if (pos == "3") togglePage(pos);
+  });
 });
 
 const slider = document.querySelector("section.pageContainer");
@@ -29,21 +32,19 @@ const slider = document.querySelector("section.pageContainer");
 let touchStart, touchEnd;
 
 slider.addEventListener(
-    'touchstart',
-    e => (touchStart = e.targetTouches[0].clientY)
-)
+  "touchstart",
+  (e) => (touchStart = e.targetTouches[0].clientY)
+);
 slider.addEventListener(
-    'touchmove',
-    e => (touchEnd = e.targetTouches[0].clientY)
-)
-slider.addEventListener('touchend', e => {
-
-    if (touchStart - touchEnd < 30 && pos > 1) {
-        pos--;
-        togglePage(pos);
-    } else if (touchStart - touchEnd > 30 && pos < 3) {
-        pos++;
-        togglePage(pos);
-    }
-
-})
+  "touchmove",
+  (e) => (touchEnd = e.targetTouches[0].clientY)
+);
+slider.addEventListener("touchend", (e) => {
+  if (touchStart - touchEnd < 30 && pos > 1) {
+    pos--;
+    togglePage(pos);
+  } else if (touchStart - touchEnd > 30 && pos < 3) {
+    pos++;
+    togglePage(pos);
+  }
+});

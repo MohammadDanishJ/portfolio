9800f1ad253f2bd713a44e60c425b2f595e62188
Mohammad Danish <mohddanish8299@gmail.com>
2022-07-25 15:02:54 +0530

    added support for previous version [https://portfolio-discarded.netlify.app]

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
         style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))' }}>We're
         Under Construction</div>
       {/* <!-- Under Construction --> */}
+
+      {/* Previous version tag */}
+      <a target='_blank' rel='noreferrer' href='https://portfolio-discarded.netlify.app' className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5">Click to visit previous version</a>
+      {/* Previous version tag */}
     </main>
   );
 }

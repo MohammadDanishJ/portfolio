a4d878336043146a35fbb4425a777858343163fc
Mohammad Danish <mohddanish8299@gmail.com>
2022-07-27 19:32:44 +0530

    added routing and make page component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-// import './App.css'
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
-    <main>
-      {/* <!-- Under Construction --> */}
-      <div className="pfx t0 pt-.5 pb-.5 pl-.5 pr-.5"
-        style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))' }}>We're
-        Under Construction</div>
-      {/* <!-- Under Construction --> */}
-
-      {/* Previous version tag */}
-      <a target='_blank' rel='noreferrer' href='https://portfolio-discarded.netlify.app' className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5">Click to visit previous version</a>
-      {/* Previous version tag */}
-    </main>
+    <Router>
+      <Routes>
+        {/* Fallback for default URL, Redirect to /home */}
+        <Route exact path="/" element={<Home />} />
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      {/* <!-- Under Construction --> */}
+      <div className="pfx t0 pt-.5 pb-.5 pl-.5 pr-.5"
+        style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))' }}>We're
+        Under Construction</div>
+      {/* <!-- Under Construction --> */}
+
+      {/* Previous version tag */}
+      <a target='_blank' rel='noreferrer' href='https://portfolio-discarded.netlify.app' className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5">Click to visit previous version</a>
+      {/* Previous version tag */}
+    </main>
+  )
+}
+
+export default Home

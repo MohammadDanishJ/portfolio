be1c10f6852f9cd73d1d459d28b56409c82f8d58
Mohammad Danish <mohddanish8299@gmail.com>
2022-07-29 21:31:00 +0530

    highlight site construction header

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,13 +5,13 @@ const Home = () => {
   return (
     <main>
       {/* <!-- Under Construction --> */}
-      <div className="pfx t0 pt-.5 pb-.5 pl-.5 pr-.5"
-        style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))' }}>We're
+      <div className="pfx t0 pt-.5 pb-.5 pl-.5 pr-.5 p12"
+        style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))', borderRadius:'0 0 20px 0', boxShadow: '0 0 20px rgb(56,56,56)'}}>We're
         Under Construction</div>
       {/* <!-- Under Construction --> */}
 
       {/* Previous version tag */}
-      <a target='_blank' rel='noreferrer' href='https://portfolio-discarded.netlify.app' className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5">Click to visit previous version</a>
+      <a target='_blank' rel='noreferrer' href='https://portfolio-discarded.netlify.app' className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5 p12"  style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))', borderRadius:'0 0 0 20px', boxShadow: '0 0 20px rgb(56,56,56)' }}>Click to visit previous version</a>
       {/* Previous version tag */}
 
       <section id="home" class="main prel fl fl-c h100vh" style={{fontSize: 102}}>Hola!</section>

ea8c431cc3f6b595d3aea034e6270dcca1706020
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-06 21:03:24 +0530

    make subtitle center

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -7,7 +7,7 @@ const LandingSection = () => {
     <section id="home" class="main prel fl fl-c fl-d-cl h100vh">
     <div className="container lhinit fl fl-c fl-d-cl">
         <div className='fl text-center' style={{fontSize: 102}}>Mohammad Danish</div>
-        <div className='fl' style={{fontSize: 18, color:'#999', letterSpacing:2, paddingTop:10}}>Web Developer | Software Engineer | React Developer</div>
+        <div className='fl text-center' style={{fontSize: 18, color:'#999', letterSpacing:2, paddingTop:10}}>Web Developer | Software Engineer | React Developer</div>
     </div>
     {/* <div className='fl fl-c fl-d-cl '> */}
       <div className='fl fl-c fl-d-cl w100' style={{fontSize: 24, color:'#666',transform: 'translateY(90px)'}}>

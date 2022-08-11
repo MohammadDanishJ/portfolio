ba699f11c5e7d1e68341f76c75564b3e23d5775b
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-11 19:07:49 +0530

    grouped different components and their stylings in single folder

--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,3 +1,3 @@
-export {default as ContactSection} from './ContactSection'
-export {default as LandingSection} from './LandingSection'
-export {default as FooterSection} from './FooterSection'
+export {default as ContactSection} from './ContactSection/'
+export {default as LandingSection} from './LandingSection/'
+export {default as FooterSection} from './FooterSection/'

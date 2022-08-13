02332bd965bebadc48c0e7392fa5d1dc0db97521
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-13 19:32:15 +0530

    added social icons/links

--- src/components/FooterSection/index.jsx
+++ src/components/FooterSection/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './styles.css'
-import {BsGithub} from 'react-icons/bs'
+import {BsGithub, BsInstagram, BsLinkedin} from 'react-icons/bs'
 const FooterSection = () => {
   return (
     <section id="Footer" className="main prel fl fl-c">
         <div className="container lhinit fl fl-c fl-d-cl">
             <div className='fl fl-c fl-d-cl'>
                 <div className='p12 text-center'>Designed and Developed by <strong>Mohammad Danish</strong></div>
-                <a href='https://github.com/MohammadDanishJ' className='fl'> <BsGithub size={16} style={{marginRight:6}}/> <span style={{color: '#6868ff'}}>MohammadDanishJ</span></a>
+                <div className='fl fl-c logo-container'>
+                  <a target='_blank' rel='noreferrer' href='https://github.com/MohammadDanishJ' className='fl logo'>
+                    <BsGithub size={24} style={{marginRight:6}}/>   
+                  </a>
+                  <a target='_blank' rel='noreferrer' href='https://www.instagram.com/mohammaddanishj/' className='fl logo'>
+                    <BsInstagram size={24} style={{marginRight:6}}/>
+                  </a>
+                  <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/mohammaddanishj/' className='fl logo'>
+                    <BsLinkedin size={24} style={{marginRight:6}}/>
+                  </a>
+                </div>
             </div>
         </div>
     </section>

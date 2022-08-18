038f9918c0f383008857fdbb5367fa6e56c878b6
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-18 20:30:45 +0530

    added sample project list and show in UI

---
+++ src/data/index.js
@@ -0,0 +1,26 @@
+export const projects = [{
+    id: 1,
+    title: 'Group',
+    description: 'Group description',
+    image: 'group.png',
+    url: '',
+    github: '',
+    technologies: ['react', 'redux', 'nodejs', 'mongodb', 'express', 'socketio', 'material-ui', 'bootstrap', 'sass', 'webpack', 'babel', 'jest', 'enzyme', 'docker', 'nginx', 'aws', 'heroku', 'git', 'github', 'vscode', 'linux', 'windows', 'macos']
+}, {
+    id: 2,
+    title: 'Portfolio',
+    description: 'Portfolio description',
+    image: 'portfolio.png',
+    url: '',
+    github: '',
+    technologies: ['react', 'redux', 'nodejs', 'mongodb', 'express', 'socketio', 'material-ui', 'bootstrap', 'sass', 'webpack', 'babel', 'jest', 'enzyme', 'docker', 'nginx', 'aws', 'heroku', 'git', 'github', 'vscode', 'linux', 'windows', 'macos']
+}, {
+    id: 3,
+    title: 'Chat',
+    description: 'Chat description',
+    image: 'chat.png',
+    url: '',
+    github: '',
+    technologies: ['react', 'redux', 'nodejs', 'mongodb', 'express', 'socketio', 'material-ui', 'bootstrap', 'sass', 'webpack', 'babel', 'jest', 'enzyme', 'docker', 'nginx', 'aws', 'heroku', 'git', 'github', 'vscode', 'linux', 'windows', 'macos']
+}
+]
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,115 @@
-import React from 'react'
-import {ContactSection, FooterSection, LandingSection} from '../../components/'
+import React from "react";
+import {
+  ContactSection,
+  FooterSection,
+  LandingSection,
+} from "../../components/";
+import { projects } from "../../data";
 
 const Home = () => {
   return (
     <main>
       {/* <!-- Under Construction --> */}
-      <div className="pfx t0 pt-.5 pb-.5 pl-.5 pr-.5 p12"
-        style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))', borderRadius:'0 0 20px 0', boxShadow: '0 0 20px rgb(56,56,56)', cursor: 'default'}}>We're
-        Under Construction</div>
+      <div
+        className="pfx t0 pt-.5 pb-.5 pl-.5 pr-.5 p12"
+        style={{
+          zIndex: 1,
+          background:
+            "linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))",
+          borderRadius: "0 0 20px 0",
+          boxShadow: "0 0 20px rgb(56,56,56)",
+          cursor: "default",
+        }}
+      >
+        We're Under Construction
+      </div>
       {/* <!-- Under Construction --> */}
 
       {/* Previous version tag */}
-      <a target='_blank' rel='noreferrer' href='https://portfolio-discarded.netlify.app' className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5 p12"  style={{ zIndex: 1, background: 'linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))', borderRadius:'0 0 0 20px', boxShadow: '0 0 20px rgb(56,56,56)' }}>Click to visit previous version</a>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://portfolio-discarded.netlify.app"
+        className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5 p12"
+        style={{
+          zIndex: 1,
+          background:
+            "linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))",
+          borderRadius: "0 0 0 20px",
+          boxShadow: "0 0 20px rgb(56,56,56)",
+        }}
+      >
+        Click to visit previous version
+      </a>
       {/* Previous version tag */}
 
       <LandingSection />
-      <ContactSection/>
-      <FooterSection/>
-    </main>
-  )
-}
 
-export default Home
+      <section className="lhinit fl fl-c fl-d-cl">
+        <p
+          style={{
+            fontSize: 18,
+            color: "#999",
+            letterSpacing: 2,
+            paddingTop: 10,
+            fontFamily: "monospace",
+          }}
+        >
+          Project List below is just a sample. [Remember, We're Under
+          Construction]
+        </p>
+        <div className="fl fl-c">
+          {projects.map((project, index) => (
+            <div
+              className="lhinit fl fl-c fl-d-cl p12"
+              key={index}
+              id={project.id}
+            >
+              <div
+                className="fl text-center"
+                style={{
+                  fontSize: 18,
+                  paddingTop: 10,
+                }}
+              >
+                {project.title}
+              </div>
+              <div
+                className="fl text-center"
+                style={{
+                  fontSize: 16,
+                  color: "#999",
+                  paddingTop: 10,
+                }}
+              >
+                {project.description}
+              </div>
+              <div
+                className="fl fl-c text-center fl-ww"
+                style={{
+                  width: 200,
+                  fontSize: 16,
+                  color: "#999",
+                  letterSpacing: 2,
+                  paddingTop: 10,
+                  fontFamily: "monospace",
+                }}
+              >
+                {project.technologies.map((technology, index) => (
+                  <div style={{ marginLeft: 2 }} key={index}>
+                    {technology},{" "}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      <ContactSection />
+      <FooterSection />
+    </main>
+  );
+};
+
+export default Home;

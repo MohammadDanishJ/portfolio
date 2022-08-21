a324854ea0405c28c689a9e040788e8961022110
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-21 21:51:27 +0530

    removed visit previous version link

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,24 +9,6 @@ import {
 const Home = () => {
   return (
     <main>
-      {/* Previous version tag */}
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://portfolio-discarded.netlify.app"
-        className="pfx t0 r0 pt-.5 pb-.5 pl-.5 pr-.5 p12"
-        style={{
-          zIndex: 1,
-          background:
-            "linear-gradient(45deg, rgba(0, 14, 42, 0.6), rgba(41, 0, 29, 0.6))",
-          borderRadius: "0 0 0 20px",
-          boxShadow: "0 0 20px rgb(56,56,56)",
-        }}
-      >
-        Click to visit previous version
-      </a>
-      {/* Previous version tag */}
-
       <LandingSection />
       <ProjectsSection />
       <ContactSection />

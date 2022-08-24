9e3b9f1550b5f5ba5ad8cac6337d1830ef178ffa
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-24 19:25:18 +0530

    updated project data for kuber

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,12 @@
 export const projects = [{
     id: 1,
-    title: 'Group',
-    description: 'Group description',
-    type: 'web',
+    title: 'Kuber Group',
+    description: 'Onlline Messaging Service Provider',
+    type: 'Web | Desktop | Mobile',
     image: 'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
     url: './',
-    github: '',
-    technologies: ['react', 'redux', 'nodejs', 'mongodb', 'express', 'socketio', 'material-ui', 'bootstrap', 'sass', 'webpack', 'babel', 'jest', 'enzyme', 'docker', 'nginx', 'aws', 'heroku', 'git', 'github', 'vscode', 'linux', 'windows', 'macos']
+    github: 'https://github.com/KuberGroup',
+    technologies: ['html', 'css', 'javascript', 'react', 'firebase', 'react native']
 }, {
     id: 2,
     title: 'Portfolio',

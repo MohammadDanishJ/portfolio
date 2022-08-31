3032b5122afc5600d36098b16bb8b1da3b229464
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-31 16:49:23 +0530

    removed unnecessary project

--- src/data/index.js
+++ src/data/index.js
@@ -16,14 +16,5 @@ export const projects = [{
     url: 'https://groupworkflow.netlify.app/',
     github: 'https://github.com/MohammadDanishJ/group',
     technologies: ['html', 'css', 'javascript', 'firebase']
-}, {
-    id: 3,
-    title: 'Chat',
-    description: 'Chat description',
-    type: 'web app',
-    image: 'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
-    url: './',
-    github: '',
-    technologies: ['react', 'redux', 'nodejs', 'mongodb', 'express', 'socketio', 'material-ui', 'bootstrap', 'sass', 'webpack', 'babel', 'jest', 'enzyme', 'docker', 'nginx', 'aws', 'heroku', 'git', 'github', 'vscode', 'linux', 'windows', 'macos']
 }
 ]

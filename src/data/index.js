674fd5d94097dc53ea875c8da72427602150e93f
Mohammad Danish <mohddanish8299@gmail.com>
2022-08-31 16:47:23 +0530

    updated project data for [group workflow]

--- src/data/index.js
+++ src/data/index.js
@@ -9,13 +9,13 @@ export const projects = [{
     technologies: ['html', 'css', 'javascript', 'react', 'firebase', 'react native']
 }, {
     id: 2,
-    title: 'Portfolio',
-    description: 'Portfolio description',
-    type: 'app',
+    title: 'Group Workflow',
+    description: 'Chat Web App',
+    type: 'PWA',
     image: 'https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
-    url: './',
-    github: '',
-    technologies: ['react', 'redux', 'nodejs', 'mongodb', 'express', 'socketio', 'material-ui', 'bootstrap', 'sass', 'webpack', 'babel', 'jest', 'enzyme', 'docker', 'nginx', 'aws', 'heroku', 'git', 'github', 'vscode', 'linux', 'windows', 'macos']
+    url: 'https://groupworkflow.netlify.app/',
+    github: 'https://github.com/MohammadDanishJ/group',
+    technologies: ['html', 'css', 'javascript', 'firebase']
 }, {
     id: 3,
     title: 'Chat',

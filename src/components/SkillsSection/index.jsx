008bcf579be143fb400f4f65bd7f76360fbd4bb8
Mohd Danish <mohddanish8299@gmail.com>
2022-12-20 16:03:19 +0530

    fixed class property to className

--- src/components/SkillsSection/index.jsx
+++ src/components/SkillsSection/index.jsx
@@ -8,120 +8,120 @@ const SkillsSection = () => {
         className="prel w100 lhinit fl fl-c fl-d-cl"
         style={{ marginRight: -15, marginLeft: -15 }}
       >
-        <div class="slider">
-          <div class="qualifications-container slide-track">
-            <div class="qualification slide">
-              <div class="content html"></div>
+        <div className="slider">
+          <div className="qualifications-container slide-track">
+            <div className="qualification slide">
+              <div className="content html"></div>
               <h1>html</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content css"></div>
+            <div className="qualification slide">
+              <div className="content css"></div>
               <h1>css</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content javascript"></div>
+            <div className="qualification slide">
+              <div className="content javascript"></div>
               <h1>javascript</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content bootstrap"></div>
+            <div className="qualification slide">
+              <div className="content bootstrap"></div>
               <h1>bootstrap</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content react"></div>
+            <div className="qualification slide">
+              <div className="content react"></div>
               <h1>react</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content firebase"></div>
+            <div className="qualification slide">
+              <div className="content firebase"></div>
               <h1>firebase</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content git"></div>
+            <div className="qualification slide">
+              <div className="content git"></div>
               <h1>git</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content jquery"></div>
+            <div className="qualification slide">
+              <div className="content jquery"></div>
               <h1>jquery</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content php"></div>
+            <div className="qualification slide">
+              <div className="content php"></div>
               <h1>php</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content mysql"></div>
+            <div className="qualification slide">
+              <div className="content mysql"></div>
               <h1>mysql</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content c-plus-plus"></div>
+            <div className="qualification slide">
+              <div className="content c-plus-plus"></div>
               <h1>c++</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content java"></div>
+            <div className="qualification slide">
+              <div className="content java"></div>
               <h1>Java</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring"></div>
+            <div className="qualification slide">
+              <div className="content spring"></div>
               <h1>Spring</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring-boot"></div>
+            <div className="qualification slide">
+              <div className="content spring-boot"></div>
               <h1>Spring Boot</h1>
             </div>
 
             {/* iter 2  */}
-            <div class="qualification slide">
-              <div class="content html"></div>
+            <div className="qualification slide">
+              <div className="content html"></div>
               <h1>html</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content css"></div>
+            <div className="qualification slide">
+              <div className="content css"></div>
               <h1>css</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content javascript"></div>
+            <div className="qualification slide">
+              <div className="content javascript"></div>
               <h1>javascript</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content bootstrap"></div>
+            <div className="qualification slide">
+              <div className="content bootstrap"></div>
               <h1>bootstrap</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content react"></div>
+            <div className="qualification slide">
+              <div className="content react"></div>
               <h1>react</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content firebase"></div>
+            <div className="qualification slide">
+              <div className="content firebase"></div>
               <h1>firebase</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content git"></div>
+            <div className="qualification slide">
+              <div className="content git"></div>
               <h1>git</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content jquery"></div>
+            <div className="qualification slide">
+              <div className="content jquery"></div>
               <h1>jquery</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content php"></div>
+            <div className="qualification slide">
+              <div className="content php"></div>
               <h1>php</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content mysql"></div>
+            <div className="qualification slide">
+              <div className="content mysql"></div>
               <h1>mysql</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content c-plus-plus"></div>
+            <div className="qualification slide">
+              <div className="content c-plus-plus"></div>
               <h1>c++</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content java"></div>
+            <div className="qualification slide">
+              <div className="content java"></div>
               <h1>Java</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring"></div>
+            <div className="qualification slide">
+              <div className="content spring"></div>
               <h1>Spring</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring-boot"></div>
+            <div className="qualification slide">
+              <div className="content spring-boot"></div>
               <h1>Spring Boot</h1>
             </div>
           </div>
@@ -129,120 +129,120 @@ const SkillsSection = () => {
 
         {/* reverse */}
 
-        <div class="slider">
-          <div class="qualifications-container slide-track slide-reverse">
-            <div class="qualification slide">
-              <div class="content html"></div>
+        <div className="slider">
+          <div className="qualifications-container slide-track slide-reverse">
+            <div className="qualification slide">
+              <div className="content html"></div>
               <h1>html</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content css"></div>
+            <div className="qualification slide">
+              <div className="content css"></div>
               <h1>css</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content javascript"></div>
+            <div className="qualification slide">
+              <div className="content javascript"></div>
               <h1>javascript</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content bootstrap"></div>
+            <div className="qualification slide">
+              <div className="content bootstrap"></div>
               <h1>bootstrap</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content react"></div>
+            <div className="qualification slide">
+              <div className="content react"></div>
               <h1>react</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content firebase"></div>
+            <div className="qualification slide">
+              <div className="content firebase"></div>
               <h1>firebase</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content git"></div>
+            <div className="qualification slide">
+              <div className="content git"></div>
               <h1>git</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content jquery"></div>
+            <div className="qualification slide">
+              <div className="content jquery"></div>
               <h1>jquery</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content php"></div>
+            <div className="qualification slide">
+              <div className="content php"></div>
               <h1>php</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content mysql"></div>
+            <div className="qualification slide">
+              <div className="content mysql"></div>
               <h1>mysql</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content c-plus-plus"></div>
+            <div className="qualification slide">
+              <div className="content c-plus-plus"></div>
               <h1>c++</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content java"></div>
+            <div className="qualification slide">
+              <div className="content java"></div>
               <h1>Java</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring"></div>
+            <div className="qualification slide">
+              <div className="content spring"></div>
               <h1>Spring</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring-boot"></div>
+            <div className="qualification slide">
+              <div className="content spring-boot"></div>
               <h1>Spring Boot</h1>
             </div>
 
             {/* iter 2  */}
-            <div class="qualification slide">
-              <div class="content html"></div>
+            <div className="qualification slide">
+              <div className="content html"></div>
               <h1>html</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content css"></div>
+            <div className="qualification slide">
+              <div className="content css"></div>
               <h1>css</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content javascript"></div>
+            <div className="qualification slide">
+              <div className="content javascript"></div>
               <h1>javascript</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content bootstrap"></div>
+            <div className="qualification slide">
+              <div className="content bootstrap"></div>
               <h1>bootstrap</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content react"></div>
+            <div className="qualification slide">
+              <div className="content react"></div>
               <h1>react</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content firebase"></div>
+            <div className="qualification slide">
+              <div className="content firebase"></div>
               <h1>firebase</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content git"></div>
+            <div className="qualification slide">
+              <div className="content git"></div>
               <h1>git</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content jquery"></div>
+            <div className="qualification slide">
+              <div className="content jquery"></div>
               <h1>jquery</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content php"></div>
+            <div className="qualification slide">
+              <div className="content php"></div>
               <h1>php</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content mysql"></div>
+            <div className="qualification slide">
+              <div className="content mysql"></div>
               <h1>mysql</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content c-plus-plus"></div>
+            <div className="qualification slide">
+              <div className="content c-plus-plus"></div>
               <h1>c++</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content java"></div>
+            <div className="qualification slide">
+              <div className="content java"></div>
               <h1>Java</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring"></div>
+            <div className="qualification slide">
+              <div className="content spring"></div>
               <h1>Spring</h1>
             </div>
-            <div class="qualification slide">
-              <div class="content spring-boot"></div>
+            <div className="qualification slide">
+              <div className="content spring-boot"></div>
               <h1>Spring Boot</h1>
             </div>
           </div>
